edeeba29f0dd0e9b3faefd32c6b918a7a6068221
Hxxnseo <seoh1999@naver.com>
2022-12-04 15:15:26 +0900

    finish

--- js/index.js
+++ js/index.js
@@ -1,17 +1,19 @@
-const keys = document.querySelectorAll(".key");
-const nowNote = document.querySelector(".playingNote");
+window.onload = function () {
+  const keys = document.querySelectorAll(".key");
+  const nowNote = document.querySelector(".playingNote");
 
-playSound = e => {
-  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+  playSound = e => {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
 
-  if (audio) {
-    const keyNote = key.getAttribute("data-note");
+    if (audio) {
+      const keyNote = key.getAttribute("data-note");
 
-    nowNote.innerHTML = keyNote;
-    audio.currentTime = 0;
-    audio.play();
-  }
+      nowNote.innerHTML = keyNote;
+      audio.currentTime = 0;
+      audio.play();
+    }
+  };
+
+  window.addEventListener("keydown", playSound);
 };
-
-window.addEventListener("keydown", playSound);
