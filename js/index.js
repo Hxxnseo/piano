647598cf349e3bbf146fda455ea454b57d9c57fa
Hxxnseo <seoh1999@naver.com>
2022-12-07 08:11:02 +0900

    fin

--- js/index.js
+++ js/index.js
@@ -2,12 +2,14 @@ window.onload = function () {
   const keys = document.querySelectorAll(".key");
   const nowNote = document.querySelector(".playingNote");
 
-  playSound = e => {
+  playNote = e => {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
 
     if (audio) {
       const keyNote = key.getAttribute("data-note");
+
+      key.classList.add("playing");
 
       nowNote.innerHTML = keyNote;
       audio.currentTime = 0;
@@ -15,5 +17,27 @@ window.onload = function () {
     }
   };
 
-  window.addEventListener("keydown", playSound);
+  removeTransition = e => {
+    keys.forEach(key => {
+      if (key.dataset.key == e.keyCode) {
+        key.classList.remove("playing");
+      }
+    });
+  };
+
+  window.addEventListener("keydown", playNote);
+  window.addEventListener("keyup", removeTransition);
 };
+
+$(function () {
+  $(".before").ripples({
+    resolution: 256,
+    perturbance: 0.02,
+  });
+
+  $(".yes").click(e => {
+    e.preventDefault();
+    $(".before").fadeOut();
+    $(".after").fadeIn();
+  });
+});
